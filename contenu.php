9abf794226a58240ddc20c3096fdc9103245e750
user <kozlova.valeriya.v@gmail.com>
2019-12-10 09:12:08 +0100

    commit

--- contenu.php
+++ contenu.php
@@ -5,20 +5,22 @@
                 <div class="col s12 m6 offset-m3 z-depth-2">
                     <!-- todo yet to do -->
                     <form action="" method="POST" class="todos">
-                        <p>
-                            <label>
-                                <input type="checkbox" />
+                    <div id="toDoList">
+                        <p draggable="true">
+                             <label>
+                                <input class="toDo" type="checkbox" />
                                 <span>Complete todo challenge</span>
                             </label>
                         </p>
-                        <p>
+                        <p draggable="true">
                             <label>
-                                <input type="checkbox" checked/>
+                                <input class="toDo" type="checkbox"/>
                                 <span>Go to sleep early</span>
                             </label>
                         </p>
+                    </div>
                         <div class="input-field left-align">
-                            <button class="btn waves-effect waves-light orange" type="submit" name="submit" value="submit" disabled>Submit</button>
+                            <button class="btn waves-effect waves-light orange" type="submit" name="submit" value="submit" id="submit" disabled>Submit</button>
                         </div>
                     </form>
                     <!-- todo completed -->
@@ -34,3 +36,4 @@
                 </div>
             </div>
         </div>
+    <script src="assets/js/script.js"></script>
